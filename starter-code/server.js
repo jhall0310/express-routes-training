0a59687629fdcc82e573c4c100b9acb4052ca27f
jhall0310 <jonathanphall@me.com>
2016-11-08 09:30:05 -0800

    pick a number working art gallery html added

--- starter-code/server.js
+++ starter-code/server.js
@@ -11,6 +11,7 @@ app.use(express.static('public'));
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
 // Allow CORS:
 // not necessary since we'll be making requests from a js file
   // that we are also serving (as static assets in public)
@@ -22,12 +23,42 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // ROUTES
 // Root Route
+app.get('/', function (req, res) {
+  // send the text 'Hello World!' when someone visits localhost:3000/
+  console.log('req = ', req);
+  console.log('res = ', res);
+
+  //res.send('Hello World!');
+  res.sendFile('views/index.html' , { root : __dirname});
+});
+
+app.get('/art-gallery', function (request, response) {
+  response.sendFile('views/art-gallery.html' , { root : __dirname});
+});
 
 
 // Gallery View Route
 
 
 // The Number Guessing Game
+app.post('/', function pickNumber(request, response) {
+var userNumber = request.body.number;
+if(userNumber < 10){
+response.send('Try again too low');
+console.log('Try again too low');
+}
+else {
+  response.send('Try again too high')
+  console.log('Nope');
+}
+
+if (userNumber === 10) {
+  response.send('Finally took you long enough')
+  console.log("Finally took you long enough");
+}
+
+});
+
 
 
 // Gallery
